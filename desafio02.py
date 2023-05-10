29805504c7e18a80e2809f56bc281dda75299ee6
ZenonKurtzDev <125083086+ZenonKurtzDev@users.noreply.github.com>
2023-05-10 13:07:08 -0300

    Justes

--- desafio02.py
+++ desafio02.py
@@ -1,4 +1,4 @@
 dia=input('Dia  =')
 mes=input('Mes = ')
 ano=input('Ano = ')
-print( ' Você nasceu  no  dia ' , dia + ' de junho ' , ' de '  , ano ,  '. Correto ?')
+print( ' Você nasceu  no  dia ' , dia , ' de junho ' , ' de '  , ano ,  '. Correto ?')
