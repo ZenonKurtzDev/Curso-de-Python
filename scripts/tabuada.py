b9532038e6ace2aeb83e56b185ac43ceef29ebe0
ZenonKurtzDev <125083086+ZenonKurtzDev@users.noreply.github.com>
2023-05-12 12:50:11 -0300

    ex8 e 9

---
+++ scripts/tabuada.py
@@ -0,0 +1,16 @@
+#Tabuada
+
+num=int(input('Digite um número para ver a sua tabúada \n'))
+print('-'*12)
+print('{}X{}={}'.format(num,1,num*1))
+print('{}x{}={}'.format(num,2,num*2))
+print('{}x{}={}'.format(num,3,num*3))
+print('{}x{}={}'.format(num,4,num*4))
+print('{}X{}={}'.format(num,5,num*5))
+print('{}x{}={}'.format(num,6,num*6))
+print('{}x{}={}'.format(num,7,num*7))
+print('{}x{}={}'.format(num,8,num*8))
+print('{}x{}={}'.format(num,9,num*9))
+print('{}x{}={}'.format(num,10,num*10))
+print('-'*12)
+
