918283158068700a60c12d83be35cd946c6e1b08
ZenonKurtzDev <125083086+ZenonKurtzDev@users.noreply.github.com>
2023-05-12 11:17:34 -0300

    ajuste

--- Aula 7 Operacao_arietimetica/ex07.py
+++ Aula 7 Operacao_arietimetica/ex07.py
@@ -1,8 +1,8 @@
-#Desenvolva um programa que leia as duas notas de um aluno e calcule e mostre sua média 
+#Desenvolva um programa que leia as duas notas de um aluno e calcule e mostre sua média
 
 nota1=float(input('Primeira nota '))
 nota2=float(input('Segunda nota'))
 média= (nota1 + nota2) /2
-print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'. format(nota1, nota2, 
+print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'. format(nota1, nota2, média)) 
                                                                
-                                                               média))
+                                                              
