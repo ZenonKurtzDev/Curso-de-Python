af93af2eb65640596117baab6958b6a35f313328
ZenonKurtzDev <125083086+ZenonKurtzDev@users.noreply.github.com>
2023-05-30 09:16:44 -0300

    player mp3

--- Modulo-1/exercicios/ex21.py
+++ Modulo-1/exercicios/ex21.py
@@ -1,7 +1,14 @@
 #Faça um programa em pyton que e reproduza o áudio de um arquivo mp3 
-import os
 import pygame
+
+pygame.mixer.init()
+
 pygame.init()
-pygame.mixer.music.load 
-pygame.music.play()
+
+pygame.mixer.music.load('D:/tocar_mp3/musica.mp3')
+
+pygame.mixer.music.play(loops=0,start=0.0)
+
 pygame.event.wait()
+input('Vamos lá')
+
