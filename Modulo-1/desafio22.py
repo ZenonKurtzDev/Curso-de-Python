5366dd5636109136c742eb86bc87b34366633b2e
ZenonKurtzDev <125083086+ZenonKurtzDev@users.noreply.github.com>
2023-05-31 10:59:00 -0300

    desafio 23

--- Modulo-1/desafio22.py
+++ Modulo-1/desafio22.py
@@ -3,9 +3,9 @@ nome = str(input('Digite seu nome completo: ')).strip()
 print('Analisando seu nome....:')
 print('Seu nome em letras maiúsculas é {}'.format(nome.upper()))
 print('Seu nome em letras minúsculas é {}'.format(nome.lower()))
-print('Seu nome tem ao todo {} letras'.format(len(nome)-nome.count(' ')))
-print('Seu primeiro tem {}letras'.format(nome.find('')))
+print('Seu nome tem ao todo {} letras'.format(len(nome)-nome.count('')))
+print('Seu primeiro tem {} letras'.format(nome.find(' ')))
 
 #Pode ser assim
 separa=nome.split()
-print('Seu primeiro nome é {}ele tem letras{}'.fomrmat(separa[0],len))
+print('Seu primeiro nome é {} ele tem letras {}'.fomrmat(separa[0],len))
